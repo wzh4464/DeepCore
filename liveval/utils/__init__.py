a3095628a27c43c243c5d4ec44546f54f3a675bd
zihan_cs11369a <32484940+wzh4464@users.noreply.github.com>
2025-05-12 10:23:16 +0800

    新增 .sourcery.yaml 配置文件以禁用冗余 f-string 的移除规则，同时新增 liveval/utils/__init__.py 文件以初始化 utils 模块，包含文件创建和修改信息。

---
+++ liveval/utils/__init__.py
@@ -0,0 +1,14 @@
+###
+# File: ./liveval/utils/__init__.py
+# Created Date: Monday, May 12th 2025
+# Author: Zihan
+# -----
+# Last Modified: Monday, 12th May 2025 10:04:22 am
+# Modified By: the developer formerly known as Zihan at <wzh4464@gmail.com>
+# -----
+# HISTORY:
+# Date      		By   	Comments
+# ----------		------	---------------------------------------------------------
+###
+
+# __init__.py
